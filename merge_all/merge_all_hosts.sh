65ef31165d9ce005293f0677cef1d3d39e06acfe
Cyanic76 <cyanic076@gmail.com>
2022-09-04 15:36:05 +0200

    Added Twitter Hosts + LICENSE update

--- merge_all/merge_all_hosts.sh
+++ merge_all/merge_all_hosts.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cat ../cyanicHosts.txt | grep -E '^0.0.0.0.*$' >> merged.txt
 cat ../corporations/* | grep -E '^0.0.0.0.*$' >> merged.txt
-echo Done && echo Your generated file is at merged.txt.
+echo Done
