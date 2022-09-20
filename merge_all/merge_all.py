5eb702d9d1b3b5bab4d3a2578eeafbaa46213ceb
Cyanic <cyanic76@noreply.codeberg.org>
2022-09-20 18:00:51 +0200

    Update py script

--- merge_all/merge_all.py
+++ merge_all/merge_all.py
@@ -1,20 +1,21 @@
 import os
-
 pathtolookfor = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'corporations'))
-tries = 0
 
+# This will create a allhosts.txt file under the "merge_all" folder:
+# merge_all/allhosts.txt
+
+# Clean the file first so it doesn't get appended
 def deleteContent(pfile):
     pfile.seek(0)
     pfile.truncate()
 
-# Clean the file first so it doesn't get appended
 with open("allhosts.txt", "w") as txt:
     deleteContent(txt)
 
 # Open each file
 for file in os.listdir(pathtolookfor):
-    if file.endswith("txt") and not file.startswith("folderlist") and tries == 0:
-        print(f'{file}')
+    if file.endswith("txt"):
+        print(f'Read {file}')
         with open(f'{pathtolookfor}/{file}', "r") as fi:
             for ln in fi:
                 if ln.startswith("0.0.0.0"):
