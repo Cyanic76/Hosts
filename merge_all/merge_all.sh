a801396fc9e0100181695c13fb0626b7c8ab2df7
Cyanic76 <cyanic076@gmail.com>
2023-03-15 10:05:54 +0100

    Update

--- merge_all/merge_all.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cat ../cyanicHosts.txt | grep -E '^0.0.0.0.*$' >> merged.txt
-cat ../corporations/* | grep -E '^0.0.0.0.*$' >> merged.txt
--- merge_all/merge_to_file.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-sudo cat ../cyanicHosts.txt | grep -E '^0.0.0.0.*$' >> /etc/hosts.txt
-sudo cat ../corporations/* | grep -E '^0.0.0.0.*$' >> /etc/hosts.txt
